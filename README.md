dd063fe3510d0f932fc9d5a1c07d807bc997bfee
AnSq <ansq00@gmail.com>
2014-04-24 01:21:23 -0600

    I think it does basically everything I set out for it to do, so I'm calling this 1.0

--- README.md
+++ README.md
@@ -1,10 +1,28 @@
-reddit-mail-notify
-==================
+# reddit-mail-notify
 
-Status bar icon and popup notification for new reddit messages. Or rather it will be.
+Status bar icon and popup notification for new reddit messages. Completely unofficial.
 
-Currently you can run it from the terminal and it will tell you how many unread messages you have. By default it will prompt you for a username and password, or you can make a `praw.ini` file that looks like this:
+## Features
+
+* Mail icon on your status bar, just like on the website
+* Icon changes color when you have new messages
+* Tooltip shows how many new messages there are, and reminds you who you're logged in as
+* Left click icon to take you to your mailbox. Right click to go to your user page
+
+## Usage
+
+Simple run the program. It takes no parameters.
+
+By default it will prompt you for a username and password, or you can make a `praw.ini` file that looks like this:
 
 	[DEFAULT]
 	user=<username>
 	pswd=<password>
+
+## Dependancies
+
+(Versions indicate what it was developed with.)
+
+* PRAW for reddit interaction (2.1.1)
+* PyGTK for status bar icon (2.24.0)
+* PyNotify for popup messages (0.1.1)
--- reddit-mail-notify.py
+++ reddit-mail-notify.py
@@ -1,9 +1,86 @@
 #!/usr/bin/python
 
+import time
+import threading
+import webbrowser
+
 import praw
+import gtk
+import gobject
+import pynotify
 
+print "loading icons...",
+loadImage = gtk.Image()
 
-reddit = praw.Reddit(user_agent="reddit-mail-notify v0.1 by /u/AnSq")
+loadImage.set_from_file("mail.png")
+mailIcon = loadImage.get_pixbuf()
+
+loadImage.set_from_file("nomail.png")
+nomailIcon = loadImage.get_pixbuf()
+print "done"
+
+print "setting up status bar...",
+icon = gtk.StatusIcon()
+icon.set_from_pixbuf(nomailIcon)
+icon.set_visible(True)
+icon.set_tooltip("loading...")
+print "done"
+
+print "logging in...",
+reddit = praw.Reddit(user_agent="reddit-mail-notify v1.0 by /u/AnSq")
 reddit.login()
-new_messages = len(list(reddit.get_unread()))
-print "New messages for %s: %d" % (reddit.user.name, new_messages)
+print "done"
+print "logged in as %s" % reddit.user.name
+
+print "setting up popup...",
+pynotify.init("reddit-mail-notify")
+notify = pynotify.Notification("New reddit mail for /u/%s" % reddit.user.name)
+notify.set_timeout(5000)
+print "done"
+
+
+class PrevCount:
+	def __init__(self):
+		self.count = 0
+
+
+prev = PrevCount()
+
+
+def poll(prev):
+	new_messages = len(list(reddit.get_unread()))
+	print "New messages for %s: %d" % (reddit.user.name, new_messages)
+	icon.set_tooltip("%d new message%s for %s" % (new_messages, "" if new_messages==1 else "s", reddit.user.name))
+	if new_messages > 0:
+		icon.set_from_pixbuf(mailIcon)
+	else:
+		icon.set_from_pixbuf(nomailIcon)
+	if new_messages > prev.count:
+		notify.show()
+	prev.count = new_messages
+	return True
+
+
+def click(ob, ev, prev):
+	prev.count = 0
+	if ev.button == 1:
+		"left click"
+		if prev.count == 0:
+			webbrowser.open("http://www.reddit.com/message/inbox")
+		else:
+			webbrowser.open("http://www.reddit.com/message/unread")
+	elif ev.button == 3:
+		"right click"
+		webbrowser.open("http://www.reddit.com/user/%s" % reddit.user.name)
+
+
+print "connecting events...",
+icon.connect("button-press-event", click, prev)
+print "done"
+
+print "registering polling function...",
+gobject.timeout_add(60000, poll, prev)
+print "done"
+
+print "entering main loop"
+gtk.main()
